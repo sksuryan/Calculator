c3a8eefa257203f056c1741f72eeef38bb25485d
Saurabh <saurabhsuryan99@hotmail.com>
2018-08-17 21:52:31 +0530

    Second Commit

--- Calculator.cpp
+++ Calculator.cpp
@@ -5,8 +5,7 @@
 
 using namespace std;
 
-struct postfix
-{
+struct postfix{
 	char ep;
 	postfix *link;
 };
@@ -14,47 +13,39 @@ struct postfix
 postfix *front,*rear,*ptr,*save;
 char exp[30];
 
-postfix *newpost(char ep)
-{
+postfix *newpost(char ep){
 	ptr = new postfix;
 	ptr->ep = ep;
 	ptr->link = NULL;
 	if(front == NULL)
 		front = rear = ptr;
-	else
-	{
+	else{
 		rear->link = ptr;
 		rear = ptr;
 	}
 }
 
-int create_postfix(postfix *pt)
-{
+int create_postfix(postfix *pt){
 	bool check = 0;
 	char operators[15];
 	operators[0] = 'a';
 	exp[0] = 'a';
 	int n = 0,ob[10],k=0;
 	int z = 0;
-	while(pt != NULL)
-	{
+	while(pt != NULL){
 		check = 0;
-		if(isdigit(pt->ep))
-		{
+		if(isdigit(pt->ep)){
 			if(exp[0] != 'a')
 				z++;
 			exp[z] = pt->ep;
-			if(isdigit(pt->link->ep))
-			{
+			if(isdigit(pt->link->ep)){
 				if(exp[0] != 'a')
 					z++;
 				exp[z] = '%';
 			}
 		}
-		else
-		{
-			if(pt->ep == '(')
-			{
+		else{
+			if(pt->ep == '('){
 				if(operators[n] != 'a')
 					n++;
 				operators[n] = pt->ep;
@@ -62,85 +53,69 @@ int create_postfix(postfix *pt)
 				ob[k] = n;
 			}
 			else
-				if(pt->ep == '/')
-				{
-					if(operators[n] == '*' || operators[n] == '/')
-					{
+				if(pt->ep == '/'){
+					if(operators[n] == '*' || operators[n] == '/'){
 						if(exp[0] != 'a')
 							z++;
 						exp[z] = operators[n];
 						operators[n] = '/';
 					}
-					else
-					{
+					else{
 						if(operators[n] != 'a')
 							n++;
 						operators[n] = pt->ep;
 					}	
 				}
 				else
-				if(pt->ep == '*')
-				{
-					if(operators[n] == '*' || operators[n] == '/')
-					{
+				if(pt->ep == '*'){
+					if(operators[n] == '*' || operators[n] == '/'){
 						if(exp[0] != 'a')
 							z++;
 						exp[z] = operators[n];
 						operators[n] = '*';
 					}
-					else
-					{
+					else{
 						if(operators[n] != 'a')
 							n++;
 						operators[n] = pt->ep;
 					}		
 				}
 				else
-				if(pt->ep == '+')
-				{
-					if(operators[n] == '*' || operators[n] == '/' || operators[n] == '-' || operators[n] == '+')
-					{
+				if(pt->ep == '+'){
+					if(operators[n] == '*' || operators[n] == '/' || operators[n] == '-' || operators[n] == '+'){
 						if(exp[0] != 'a')
 							z++;
 						exp[z] = operators[n];
 						operators[n] = '+';
 					}
-					else
-					{
+					else{
 						if(operators[n] != 'a')
 							n++;
 						operators[n] = pt->ep;
 					}
 				}
 				else
-				if(pt->ep == '-')
-				{
-					if(operators[n] == '*' || operators[n] == '/' || operators[n] == '-' || operators[n] == '+')
-					{
+				if(pt->ep == '-'){
+					if(operators[n] == '*' || operators[n] == '/' || operators[n] == '-' || operators[n] == '+'){
 						if(exp[0] != 'a')
 							z++;
 						exp[z] = operators[n];
 						operators[n] = '-';
 					}
-					else
-					{
+					else{
 						if(operators[n] != 'a')
 							n++;
 						operators[n] = pt->ep;
 					}
 				}
 				else
-				if(pt->ep == ')')
-				{
-					for(int i = n; i>=ob[k];--i)
-					{
-						if(operators[i] == '(')
-						{
+				if(pt->ep == ')'){
+					for(int i = n; i>=ob[k];--i){
+						if(operators[i] == '('){
 							--n;
 							break;
 						}
-						else
-						{
+						else{
 							if(exp[0] != 'a')
 								z++;
 							exp[z] = operators[i];
@@ -149,10 +124,8 @@ int create_postfix(postfix *pt)
 					}
 					--k;
 				}
-				for(int i = n; i>0; --i)
-				{
-					if((operators[i] == '+' || operators[i] == '-')&&(operators[i-1] == '+' || operators[i-1] == '-'))
-					{
+				for(int i = n; i>0; --i){
+					if((operators[i] == '+' || operators[i] == '-')&&(operators[i-1] == '+' || operators[i-1] == '-')){
 						if(exp[0] != 'a')
 							z++;
 						exp[z] = operators[i-1];
@@ -163,14 +136,12 @@ int create_postfix(postfix *pt)
 		}
 		pt = pt->link;
 	}
-	for(int i =n;i>=0;--i)
-	{
+	for(int i =n;i>=0;--i){
 		if(exp[0] != 'a')
 			z++;
 		exp[z] = operators[i];
 	}
-	while(front != rear)
-	{
+	while(front != rear){
 		save = front;
 		front = front->link;
 		delete save;
@@ -180,47 +151,38 @@ int create_postfix(postfix *pt)
 	return z;
 }
 
-void evaluate(int count)
-{
+void evaluate(int count){
 	int n = 0,n1=0,x;
 	float cal[30];
 	bool flow = 0;
-	while(n1 <= count)
-	{
-		if(exp[n1] == '/')
-		{
+	while(n1 <= count){
+		if(exp[n1] == '/'){
 			cal[n-1] = cal[n-1]/cal[n];
 			--n;
 		}
 		else
-		if(exp[n1] == '*')
-		{
+		if(exp[n1] == '*'){
 			cal[n-1] = cal[n-1]*cal[n];
 			--n;
 		}
 		else
-		if(exp[n1] == '+')
-		{
+		if(exp[n1] == '+'){
 			cal[n-1] = cal[n-1]+cal[n];
 			--n;
 		}
 		else
-		if(exp[n1] == '-')
-		{
+		if(exp[n1] == '-'){
 			cal[n-1] = cal[n-1]-cal[n];
 			--n;
 		}
 		else
-		if(exp[n1] == '%')
-		{
+		if(exp[n1] == '%'){
 			x = exp[n1+1]-'0';
 			cal[n] = cal[n]*10+x;
 			n1++;
 		}
-		else
-		{
-			if(flow == 0)
-			{
+		else{
+			if(flow == 0){
 				flow = 1;
 			}
 			else
@@ -232,22 +194,18 @@ void evaluate(int count)
 	cout<<"\t\tAnswer :- "<<cal[0]<<endl;
 }
 
-int main()
-{
+int main(){
 	char ep,ch;
 	int count = 0;
 	cout<<"\t\t\t\t\t\t--Calculator--"<<endl;
 	cout<<"\n\t Use ';' to end the calculation Part.."<<endl;
-	do
-	{
+	do{
 		cout<<"\t\t--> ";
-		while(ep != ';')
-		{
+		while(ep != ';'){
 			cin>>ep;
 			newpost(ep);
 		}
-		if(ep == ';')
-		{
+		if(ep == ';'){
 			count = create_postfix(front);
 			evaluate(count);
 		}
